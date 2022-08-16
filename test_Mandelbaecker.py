f774d9b970fef608483ff502b777138ca24689f3
Lmashni <49257148+Lmashni@users.noreply.github.com>
2022-08-16 11:00:13 +0200

    Update test_Mandelbaecker.py

--- test_Mandelbaecker.py
+++ test_Mandelbaecker.py
@@ -1,8 +1,5 @@
-from Mandelbaecker import C
+from mandelbaecker.mandelbaecker import C
 import numpy as np
 
 def test_C():
-	'''
-	hi
-	'''
 	assert (C(0,2,2) == np.array([[-2.-2.j,  2.-2.j], [-2.+2.j,  2.+2.j]])).prod()
